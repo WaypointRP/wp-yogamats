a8a466d519d51143a510be3f4c15f296d4c22a14
BackSH00TER <18689469+BackSH00TER@users.noreply.github.com>
2025-04-19 00:52:59 -0700

    Add qbx support (#1)

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,18 +1,22 @@
-fx_version 'cerulean'
-game 'gta5'
+fx_version "cerulean"
+game "gta5"
 
-description 'Waypoint Yogamats'
-author 'BackSH00TER - Waypoint RP'
-version '1.0.0'
+description "Waypoint Yogamats"
+author "BackSH00TER - Waypoint RP"
+version "1.0.1"
 
 shared_script {
     -- '@ox_lib/init.lua', -- Uncomment this if you are planning to use any ox scripts (such as ox notify)
-    'shared/config.lua',
-    'shared/framework.lua',
+    "shared/config.lua",
+    "shared/framework.lua",
 }
 
 client_scripts {
-    'client.lua',
+    "client.lua",
 }
 
-lua54 'yes'
+dependencies {
+    "wp-placeables",
+}
+
+lua54 "yes"
--- shared/config.lua
+++ shared/config.lua
@@ -1,13 +1,30 @@
 Config = {}
 
-Config.Framework = 'qb' -- supports 'qb' or 'esx'
-Config.Notify = 'qb' -- supports 'qb', 'esx', 'ox' (if using ox enable @ox_lib/init.lua in the manifest)
+------------------------------------
+-- FRAMEWORK / SCRIPT CONFIGURATION
+-- Adjust these settings to match the framework and scripts you are using
+-- Note: If using ox for any option, enable @ox_lib/init.lua in the manifest!
+------------------------------------
+
+--- @type "qb" | "qbx" | "esx"
+Config.Framework = "qb"
+
+-- The notification script you are using.
+--- @type "qb" | "esx" | "ox" | "none"
+Config.Notify = "qb"
+
+------------------------------------
+--- END FRAMEWORK / SCRIPT CONFIGURATION
+------------------------------------
 
 -- Set to true to periodically reduce stress while doing yoga
+--- @type boolean
 Config.ShouldReduceStress = true
 
 -- Set to true to periodically heal while doing yoga
+--- @type boolean
 Config.ShouldIncreaseHealth = true
 
 -- How often to apply buffs while doing yoga (in milliseconds)
+--- @type number
 Config.BuffInterval = 10000
